3a8f2dc8d9f2212f93ff3e1c17433afa03613bd9
louei <djloueigenita@gmail.com>
2021-09-17 16:29:48 +0800

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PRELIM PROJECT</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+
+</head>
+<body>
+
+    <h1>HOME PAGE</h1>
+    HOME | <a class="p-2"<a href="{{url('/profile')}}">Profile</a>
+    <a href="{{url('/contact')}}">Contact</a>
+
+
+</body>
+</html>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PRELIM PROJECT</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body class ="">
+
+    <h1>PROFILE PAGE</h1>
+    PROFILE | <a class="p-2" href="{{url('/')}}">Home</a>
+    <a class="p-2" href="{{url('/profile')}}">Contact</a>
+    </div>
+
+</body>
+</html>
